8c9bcea3fff4fc3d27a2d105145dc45336ae6d16
Aquila <aquilaaregbesola@gmail.com>
2026-05-05 15:18:56 +0100

    fix smtp railway mail issue

--- app/__init__.py
+++ app/__init__.py
@@ -43,8 +43,8 @@ def create_app():
     # MAIL CONFIG
     # ======================
     app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
-    app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", 587))
-    app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS") == "True"
+    app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT") or 587)
+    app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS", "True").lower() == "true"
     app.config["MAIL_USE_SSL"] = False
     app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
     app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
